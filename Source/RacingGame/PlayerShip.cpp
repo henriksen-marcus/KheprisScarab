db8d711fa059ae5166a7960893243e1fc2e381c1
henriksen-marcus <89453098+henriksen-marcus@users.noreply.github.com>
2022-03-16 00:14:57 +0100

    Raycasting Demo

---
+++ Source/RacingGame/PlayerShip.cpp
@@ -0,0 +1,407 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerShip.h"
+//#include "GameFramework/PlayerInput.h"
+//#include "Components/InputComponent.h"
+//#include "Engine/World.h"
+//#include "Camera/CameraActor.h"
+//#include "Engine/Engine.h"
+//#include "Math/UnrealMathUtility.h"
+//#include "Kismet/KismetMathLibrary.h"
+#include "DrawDebugHelpers.h"
+//#include "Particles/ParticleSystemComponent.h"
+#include "GenericPlatform/GenericPlatformMisc.h"
+
+// Sets default values
+APlayerShip::APlayerShip()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RtRpl = CreateDefaultSubobject<UBoxComponent>(TEXT("RootReplacement"));
+	SetRootComponent(RtRpl);
+	//RtRpl->SetSimulatePhysics(true);
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
+	BaseMesh->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+	BaseMesh->SetupAttachment(GetRootComponent());
+	BaseMesh->OnComponentBeginOverlap.AddDynamic(this, &APlayerShip::OnOverlapBegin);
+
+	DashTimer = 2.f;
+	MaxSpeedBoost = 4.f;
+	SpeedBoost = 1.f;
+	InitialArmLength = 1000.f;
+	MaxHealth = 300.f;
+	Health = MaxHealth;
+	InitialMaterial = BaseMesh->GetMaterial(0);
+	bIsDashing = false;
+	bIsJumping = false;
+	EnemyCooldownTime = 1.f;
+	IgnoreInput = false;
+	GameWon = false;
+	LocalMove = FVector::ZeroVector;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetRelativeRotation(FRotator(-30.f, 0.f, 0.f));
+	SpringArm->SetUsingAbsoluteRotation(true);
+	SpringArm->TargetArmLength = InitialArmLength;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 25.f; // Lower = More delay
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->bUsePawnControlRotation = false;
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->SetRelativeRotation(FRotator(10.f, 0.f, 0.f));
+
+	UArrowComponent* Thrust1 = CreateDefaultSubobject<UArrowComponent>(TEXT("Thrust1"));
+	UArrowComponent* Thrust2 = CreateDefaultSubobject<UArrowComponent>(TEXT("Thrust2"));
+	UArrowComponent* Thrust3 = CreateDefaultSubobject<UArrowComponent>(TEXT("Thrust3"));
+	UArrowComponent* Thrust4 = CreateDefaultSubobject<UArrowComponent>(TEXT("Thrust4"));
+
+	Thrust1->SetRelativeLocationAndRotation(FVector(354.f, -432.f, -23.f), FRotator(-95.f, 0.f, 0.f));
+	Thrust2->SetRelativeLocationAndRotation(FVector(354.f, 432.f, -23.f), FRotator(-95.f, 0.f, 0.f));
+	Thrust3->SetRelativeLocationAndRotation(FVector(-66.f, -416.f, 82.f), FRotator(-95.f, 0.f, 0.f));
+	Thrust4->SetRelativeLocationAndRotation(FVector(-66.f, 416.f, 82.f), FRotator(-95.f, 0.f, 0.f));
+
+	ThrustLocations.Reserve(4);
+	ThrustLocations.Add(Thrust1);
+	ThrustLocations.Add(Thrust2);
+	ThrustLocations.Add(Thrust3);
+	ThrustLocations.Add(Thrust4);
+
+	for (int i{}; i < ThrustLocations.Num(); i++) { ThrustLocations[i]->SetupAttachment(GetRootComponent()); }
+
+	ThrustFX1 = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ThrustFX1"));
+	ThrustFX2 = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ThrustFX2"));
+	ThrustFX3 = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ThrustFX3"));
+	ThrustFX4 = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ThrustFX4"));
+	
+	/**
+	* The reason I am doing it like this instead of using a TArray is that UE crashes when using
+	* UParticleSystemComponent->Activate() since the UParticleSystemComponent is NULL.
+	* Don't know if this is a bug, but this works at least
+	*/
+	ThrustFX1->SetupAttachment(ThrustLocations[0]);
+	ThrustFX2->SetupAttachment(ThrustLocations[1]);
+	ThrustFX3->SetupAttachment(ThrustLocations[2]);
+	ThrustFX4->SetupAttachment(ThrustLocations[3]);
+
+	ThrustFX1->bAutoActivate = false;
+	ThrustFX2->bAutoActivate = false;
+	ThrustFX3->bAutoActivate = false;
+	ThrustFX4->bAutoActivate = false;
+
+	DeathFXComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("DeathFX"));
+	DeathFXComponent->SetupAttachment(GetRootComponent());
+	DeathFXComponent->bAutoActivate = false;
+	DeathFXComponent->SetRelativeScale3D(FVector(7.f, 7.f, 7.f));
+}
+
+
+void APlayerShip::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialLocation = GetActorLocation();
+
+	/** Apply effects to all subobjects, so you don't have to assign 4 individual values in blueprints */
+	if (ThrustFX)
+	{
+		ThrustFX1->SetTemplate(ThrustFX);
+		ThrustFX2->SetTemplate(ThrustFX);
+		ThrustFX3->SetTemplate(ThrustFX);
+		ThrustFX4->SetTemplate(ThrustFX);
+	}
+
+	if (DeathFX)
+	{
+		DeathFXComponent->SetTemplate(DeathFX);
+	}
+
+	//Force.Z = RtRpl->GetMass() * 100 * 9.80665;
+}
+
+
+void APlayerShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	ShootTimer += DeltaTime;
+
+	///** Ship movement - Changed to physics based */ 
+	//FVector CombinedVectors = FVector::ZeroVector;
+	//CombinedVectors += Force.X * GetActorForwardVector();
+	//CombinedVectors += Force.Y * GetActorRightVector();
+	//CombinedVectors *= 20000000 * SpeedBoost;
+	//if (JumpCurve)
+	//{
+	//	CombinedVectors.Z = ForceChange * Force.Z;
+	//}
+	//
+
+	//RtRpl->AddForce(CombinedVectors);
+	//RtRpl->SetPhysicsAngularVelocityInDegrees(FVector::ZeroVector);
+
+	//FVector Resistance = FVector::ZeroVector;
+	//Resistance = RtRpl->GetPhysicsLinearVelocity() * -1000.f;
+	//Resistance.Z = 0;
+	//RtRpl->AddForce(Resistance);
+
+	BaseMesh->SetRelativeRotation(FRotator(NextPitchPosition, 0.f, NextRollPosition));
+
+	FVector CurLoc = GetActorLocation();
+
+	//float temp2 = FMath::Abs(0.049f * CurLoc.Z - TargetZ + 1.f);
+	//float CurveStr = FMath::Clamp(temp2, 1.f, 50.f);
+	//float CurveStr = FMath::Abs(1000 / CurLoc.Z - TargetZ);
+	//UE_LOG(LogTemp, Warning, TEXT("CurveStr: %f\nHeight: %f"), CurveStr, CurLoc.Z - TargetZ)
+
+	CurLoc.Z = FMath::FInterpTo(CurLoc.Z, TargetZ, DeltaTime, 3.f);
+	SetActorLocation(CurLoc);
+
+	AddActorLocalOffset(LocalMove * DeltaTime * 110.f, true);
+
+	SetActorRotation(FRotator(0.f, NextYawPosition, 0.f));
+
+	/** Springarm rotation */
+	FRotator SpringArmRotation = SpringArm->GetRelativeRotation();
+	FRotator NewRot = FMath::RInterpTo(SpringArmRotation, GetActorRotation(), DeltaTime, 25.f);
+	NewRot.Pitch = SpringArmRotation.Pitch;
+	NewRot.Roll = 0;
+	SpringArm->SetWorldRotation(NewRot);
+
+
+	/** Raycasting */
+	FHitResult OutHit;
+	FVector Start = GetActorLocation();
+	FVector End = Start;
+	End.Z -= 10000.f;
+	FCollisionQueryParams CollisionParams;
+	bool BelowNormal = false;
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 2, 0, 2);
+
+	if (GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, CollisionParams))
+	{
+		if (OutHit.bBlockingHit)
+		{
+			//UE_LOG(LogTemp, Warning, TEXT("\nStart Z: %f\nImpact Z: %f\nDiff: %f"), Start.Z, OutHit.ImpactPoint.Z, Start.Z - OutHit.ImpactPoint.Z)
+
+			//ForceChange = 3.f;
+			TargetZ = OutHit.ImpactPoint.Z + 1000.f;
+			BelowNormal = true;
+		}
+	}
+	if (!BelowNormal)
+	{
+		//ForceChange = 0.5f;
+		//LocalMove.Z = -1.f;
+		TargetZ = 0.f;
+	}
+	
+}
+
+
+void APlayerShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	check(PlayerInputComponent != nullptr);
+
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InitializeDefaultPawnInputBinding();
+
+	PlayerInputComponent->BindAxis("Roll", this, &APlayerShip::Roll);
+	PlayerInputComponent->BindAxis("Pitch", this, &APlayerShip::Pitch);
+
+	PlayerInputComponent->BindAxis("CameraPitch", this, &APlayerShip::CameraPitch);
+	PlayerInputComponent->BindAxis("Yaw", this, &APlayerShip::Yaw);
+
+	PlayerInputComponent->BindAction("Dash", EInputEvent::IE_Pressed, this, &APlayerShip::Dash);
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &APlayerShip::Jump);
+	PlayerInputComponent->BindAction("Esc", EInputEvent::IE_Pressed, this, &APlayerShip::EscPressed);
+}
+
+
+/** Hardcode input mappings */
+void APlayerShip::InitializeDefaultPawnInputBinding()
+{
+	static bool BindingsAdded = false;
+
+	if (BindingsAdded == false)
+	{
+		BindingsAdded = true;
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("Pitch", EKeys::W, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("Pitch", EKeys::S, -1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("Roll", EKeys::A, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("Roll", EKeys::D, -1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("CameraPitch", EKeys::MouseY, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("Yaw", EKeys::MouseX, 1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("Shoot", EKeys::LeftMouseButton, 1.f));
+
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Jump", EKeys::SpaceBar));
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Reload", EKeys::R));
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Dash", EKeys::LeftShift));
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Esc", EKeys::Escape));
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Tab", EKeys::Tab));
+	}
+}
+
+
+// -------------------------------------- CUSTOM FUNCTIONS -------------------------------------- //
+
+void APlayerShip::Pitch(float Value)
+{
+	if (IgnoreInput) { return; }
+
+	// Determine if there is input
+	bPitchHasInput = !(Value == 0);
+	// If there is input, set rotation target to -25/25 based on input value, else set target to 0
+	float TargetPitch = bPitchHasInput ? Value > 0.f ? -25.0f : 25.0f : 0.f;
+
+	// Interpolate rotation towards target
+	//NextPitchPosition = FMath::FInterpTo(GetActorRotation().Pitch, TargetPitch, GetWorld()->GetDeltaSeconds(), 6.0f);
+	NextPitchPosition = FMath::FInterpTo(BaseMesh->GetRelativeRotation().Pitch, TargetPitch, GetWorld()->GetDeltaSeconds(), 6.0f);
+
+	float TargetXSpeed = bPitchHasInput ? (Value * 40.f * SpeedBoost) : 0.f;
+	LocalMove.X = FMath::FInterpTo(LocalMove.X, TargetXSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
+
+	Force.X = bPitchHasInput ? Value : 0.f;
+}
+
+
+void APlayerShip::Roll(float Value)
+{
+	if (IgnoreInput) { return; }
+
+	// Determine if there is input
+	bRollHasInput = !(Value == 0);
+	// If there is input, set rotation target to -30/30 based on input value, else set target to 0
+	float TargetRoll = bRollHasInput ? Value > 0 ? -30.0f : 30.0f : 0.f;
+	// Interpolate rotation towards target
+	NextRollPosition = FMath::FInterpTo(BaseMesh->GetRelativeRotation().Roll, TargetRoll, GetWorld()->GetDeltaSeconds(), 7.5f);
+
+	float TargetYSpeed = bRollHasInput ? (Value * -30.f) : 0.f;
+	LocalMove.Y = FMath::FInterpTo(LocalMove.Y, TargetYSpeed, GetWorld()->GetDeltaSeconds(), 2.f);
+
+	Force.Y = bRollHasInput ? Value * (-1.f) : 0.f;
+}
+
+
+void APlayerShip::Yaw(float Value)
+{
+	if (IgnoreInput) { return; }
+	NextYawPosition = GetActorRotation().Yaw + Value * GetWorld()->GetDeltaSeconds() * 50.f;
+}
+
+
+void APlayerShip::CameraPitch(float Value)
+{
+	if (!Value || IgnoreInput) { return; }
+	FRotator CurrentRot = SpringArm->GetRelativeRotation();
+	float TargetPitch = FMath::Clamp(CurrentRot.Pitch + Value * 15.f, -50.f, 0.f);
+	CurrentRot.Pitch = FMath::FInterpTo(CurrentRot.Pitch, TargetPitch, GetWorld()->GetDeltaSeconds(), 10.f);
+
+	SpringArm->SetRelativeRotation(CurrentRot);
+}
+
+
+void APlayerShip::Dash() 
+{
+	if (IgnoreInput) { return; }
+
+	if (bIsDashing) 
+	{
+		return;
+	}
+	
+	SpeedBoost = MaxSpeedBoost;
+	bIsDashing = true;
+
+	FTimerHandle handle;
+	GetWorld()->GetTimerManager().SetTimer(handle, this, &APlayerShip::ResetDash, DashTimer, false);
+
+	if (ThrustFX)
+	{
+		ThrustFX1->ResetParticles();
+		ThrustFX2->ResetParticles();
+		ThrustFX3->ResetParticles();
+		ThrustFX4->ResetParticles();
+
+		ThrustFX1->Activate();
+		ThrustFX2->Activate();
+		ThrustFX3->Activate();
+		ThrustFX4->Activate();
+	}
+}
+
+
+void APlayerShip::ResetDash()
+{
+	SpeedBoost = 1.f;
+	bIsDashing = false;
+}
+
+
+void APlayerShip::Jump()
+{
+	if (IgnoreInput) { return; }
+
+	if (bIsJumping)
+	{
+		return;
+	}
+
+	//bIsJumping = true;
+	RtRpl->AddImpulse(FVector(0.f, 0.f, 10000000.f));
+	bIsJumping = true;
+
+	FTimerHandle handle;
+	GetWorld()->GetTimerManager().SetTimer(handle, this, &APlayerShip::JumpEnd, 4.f, false);
+
+	if (ThrustFX)
+	{
+		ThrustFX1->ResetParticles();
+		ThrustFX2->ResetParticles();
+		ThrustFX3->ResetParticles();
+		ThrustFX4->ResetParticles();
+
+		ThrustFX1->Activate();
+		ThrustFX2->Activate();
+		ThrustFX3->Activate();
+		ThrustFX4->Activate();
+	}
+}
+
+
+void APlayerShip::JumpEnd()
+{
+	bIsJumping = false;
+}
+
+
+void APlayerShip::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherbodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == this || !OtherComponent || IgnoreInput) { return; }
+
+	
+}
+
+
+FVector APlayerShip::GetLoc()
+{
+	return GetActorLocation();
+}
+
+
+void APlayerShip::EscPressed()
+{
+	FGenericPlatformMisc::RequestExit(false);
+}
+
+
---
+++ Source/RacingGame/PlayerShip.h
@@ -0,0 +1,148 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "PlayerShip.generated.h"
+
+UCLASS()
+class RACINGGAME_API APlayerShip : public APawn
+{
+	GENERATED_BODY()
+public:
+	APlayerShip();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	static void InitializeDefaultPawnInputBinding();	
+
+
+public:
+
+	/** Returns the location of the playership */
+	UFUNCTION(Category = "OpenFunctions")
+	FVector GetLoc();
+
+	/** If true, freezes the player and prompts the HUD to display a win message */
+	UPROPERTY(BlueprintReadWrite, Category = "OpenVariables")
+	bool GameWon;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "PlayerMesh")
+	UBoxComponent* RtRpl; // Root replacement
+
+	UPROPERTY(EditAnywhere, Category = "PlayerMesh")
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(EditAnywhere, Category = "CameraVariables")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere, Category = "CameraVariables")
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UCurveFloat* DistanceCurve;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UCurveFloat* JumpCurve;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	int MaxAmmo;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	float DashTimer;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	float MaxSpeedBoost;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	float MaxHealth;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UMaterialInterface* DamageMaterial;
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<UArrowComponent*> ThrustLocations;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystemComponent* ThrustFX1;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystemComponent* ThrustFX2;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystemComponent* ThrustFX3;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystemComponent* ThrustFX4;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystemComponent* DeathFXComponent;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystem* ThrustFX;
+
+	UPROPERTY(EditAnywhere, Category = "EditableVariables")
+	UParticleSystem* DeathFX;
+
+	UPROPERTY()
+	UMaterialInterface* InitialMaterial;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherbodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	FVector InitialLocation;
+
+	/** Player input */
+	void Roll(float Value);
+	void Pitch(float Value);
+
+	void CameraPitch(float Value);
+	void Yaw(float Value);
+
+	void Dash();
+	void ResetDash();
+	void Jump();
+	void JumpEnd();
+	void EscPressed();
+
+	bool bPitchHasInput;
+	bool bRollHasInput;
+
+	float NextRollPosition;
+	float NextPitchPosition;
+	float NextYawPosition;
+
+	/** Used in AddActorLocalOffset() in Tick()*/
+	FVector LocalMove;
+	float TargetZ{};
+
+	float SpeedBoost;
+	float ShootTimer{};
+
+	float CurrentYaw;
+	float InitialArmLength;
+	float Health;
+	bool bIsDashing;
+	float EnemyCooldownTime;
+	bool bIsJumping;
+	bool IgnoreInput;
+	bool temp = false;
+	float ForceChange = 1.f;
+
+	TArray<float> Timers;
+
+	FVector Force = FVector::ZeroVector;
+};
